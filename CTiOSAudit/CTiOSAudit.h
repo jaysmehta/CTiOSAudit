e2689bbef70d400123aa7acb7d52637fc1169d76
jaymehta <jaysmehta91@gmail.com@example.com>
2020-08-12 14:16:17 +0530

    Initial Commit

---
+++ CTiOSAudit/CTiOSAudit.h
@@ -0,0 +1,19 @@
+//
+//  CTiOSAudit.h
+//  CTiOSAudit
+//
+//  Created by Jay Mehta on 12/08/20.
+//  Copyright © 2020 Jay Mehta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CTiOSAudit.
+FOUNDATION_EXPORT double CTiOSAuditVersionNumber;
+
+//! Project version string for CTiOSAudit.
+FOUNDATION_EXPORT const unsigned char CTiOSAuditVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTiOSAudit/PublicHeader.h>
+
+
